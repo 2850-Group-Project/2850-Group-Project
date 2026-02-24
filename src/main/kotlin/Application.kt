d3af4ed29b608573760851db9e3e875ad721ae5f
X100 <fy23wc@leeds.ac.uk>
2026-02-24 21:51:50 +0000

    1

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -6,8 +6,6 @@ import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import io.ktor.server.pebble.*
 import io.ktor.server.routing.*
-import io.pebbletemplates.pebble.PebbleEngine
-import io.pebbletemplates.pebble.loader.ClasspathLoader
 import routes.authRoutes
 
 fun main(args: Array<String>): Unit =
@@ -17,15 +15,7 @@ fun Application.module() {
 
     DatabaseFactory.init()
 
-    install(Pebble) {
-        engine = PebbleEngine.Builder()
-            .loader(
-                ClasspathLoader().apply {
-                    prefix = "templates"
-                }
-            )
-            .build()
-    }
+    install(Pebble)
 
     routing {
         authRoutes()
