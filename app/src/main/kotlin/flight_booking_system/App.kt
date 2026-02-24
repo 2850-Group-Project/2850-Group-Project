1e12b3313b3276da09ba6fe13dd0f8ed021daafc
Patrick Butuza <patrickbutuza@gmail.com>
2026-02-24 21:22:24 +0000

    resolved kotlin package error

--- app/src/main/kotlin/flight_booking_system/App.kt
+++ app/src/main/kotlin/flight_booking_system/App.kt
@@ -1,5 +1,13 @@
 package flight_booking_system
 
+import database.DBFactory
+
 fun main() {
+
+    // EXAMPLE usage of using the DBFactory to create a connection
+    println("Initialising DB connection...")
+    DBFactory.init()
+    println("Connected to DB")
+
     println("Hello world")
 }
--- app/src/main/resources/database_system/Tables.kt
+++
@@ -1,5 +0,0 @@
-package tables
-
-import org.jetbrains.exposed.sql.Table
-import org.jetbrains.exposed.sql.Column
-
