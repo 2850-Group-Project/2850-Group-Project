dd877161eb71bc1e15f2d65f38bce61f1d2cd1ec
X100 <fy23wc@leeds.ac.uk>
2026-02-25 00:47:25 +0000

    1

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,10 +23,15 @@ dependencies {
     implementation("io.ktor:ktor-server-netty:2.3.7")
     implementation("io.ktor:ktor-server-pebble:2.3.7")
     implementation("io.ktor:ktor-server-content-negotiation:2.3.7")
+
     implementation("com.h2database:h2:2.2.224")
+
     implementation("org.jetbrains.exposed:exposed-core:0.43.0")
     implementation("org.jetbrains.exposed:exposed-dao:0.43.0")
     implementation("org.jetbrains.exposed:exposed-jdbc:0.43.0")
+
     implementation("org.mindrot:jbcrypt:0.4")
-    testImplementation("org.jetbrains.kotlin:kotlin-test")
+
+    implementation("org.xerial:sqlite-jdbc:3.43.0.0")
+    implementation("org.slf4j:slf4j-simple:2.0.7")
 }
