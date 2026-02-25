1f108d5d3e1aebfddafd8ea9f1e1bed80da3d4d2
X100 <fy23wc@leeds.ac.uk>
2026-02-25 02:26:09 +0000

    1

--- src/main/kotlin/com/flightbooking/Application.kt
+++ src/main/kotlin/com/flightbooking/Application.kt
@@ -1,18 +1,12 @@
 package com.flightbooking
 
 import com.flightbooking.database.DBFactory
-import com.flightbooking.routes.authRoutes
+import io.ktor.server.application.*
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
-import io.ktor.server.application.*
-import io.ktor.server.routing.*
 import io.ktor.server.pebble.*
-import io.ktor.server.plugins.callloging.*
-import io.ktor.server.plugins.statuspages.*
-import io.ktor.server.plugins.contentnegotiation.*
-import io.ktor.server.sessions.*
-import io.ktor.server.response.*
-import org.slf4j.event.Level
+import io.ktor.server.routing.*
+import com.flightbooking.routes.authRoutes
 
 fun main() {
     val port = System.getenv("PORT")?.toIntOrNull() ?: 8080
@@ -23,38 +17,9 @@ fun main() {
 }
 
 fun Application.module() {
-    install(CallLogging) {
-        level = Level.INFO
-    }
-    install(StatusPages) {
-        exception<Throwable> { call, cause ->
-            cause.printStackTrace()
-            call.respondText("Internal Server Error: ${cause::class.simpleName}: ${cause.message}", status = io.ktor.http.HttpStatusCode.InternalServerError)
-        }
-    }
-    install(ContentNegotiation)
-    install(Pebble) {
-        cacheActive = false
-    }
-    install(Sessions) {
-        cookie<String>("COMP2850_SESSION") {
-            cookie.path = "/"
-            cookie.httpOnly = true
-        }
-    }
-    try {
-        DBFactory.init()
-    } catch (e: Throwable) {
-        e.printStackTrace()
-        throw e
-    }
+    DBFactory.init()
+    install(Pebble)
     routing {
-        get("/") {
-            call.respondText("root ok")
-        }
-        get("/__health") {
-            call.respondText("ok")
-        }
         authRoutes()
     }
 }
