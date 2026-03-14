787d900137c70e9884b415a8505bc0fa11f1f5a3
Patrick Butuza <patrickbutuza@gmail.com>
2026-03-14 22:35:07 +0000

    fuzzy search for incorrect inputs

--- src/main/kotlin/com/flightbooking/routes/PagesRoutes.kt
+++ src/main/kotlin/com/flightbooking/routes/PagesRoutes.kt
@@ -24,7 +24,7 @@ fun Route.pagesRoutes() {
     get("/home") {
         // need to add check to make sure user is logged in before loading the home page
         val session = call.sessions.get<UserSession>()
-        println(session)
+        // println(session)
 
         if (session == null) {
             call.respondRedirect("/login")
@@ -44,9 +44,9 @@ fun Route.pagesRoutes() {
         // we also need to check that all the required data is provided
         val session = call.sessions.get<UserSession>()
         
-        println("SESSION DATA VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV")
-        println(session)
-        println("SESSION DATA ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^")
+        // println("SESSION DATA VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV")
+        // println(session)
+        // println("SESSION DATA ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^")
         
         if (session == null) {
             call.respondRedirect("/login")
@@ -56,8 +56,8 @@ fun Route.pagesRoutes() {
         // packaging all search data into one class
         val search = FlightSearch(
             tripType = call.request.queryParameters["trip_type"],
-            origin = call.request.queryParameters["origin"],
-            destination = call.request.queryParameters["destination"],
+            origin = AirportTableAccess().getCityByOrigin(call.request.queryParameters["origin"] ?: ""),
+            destination = AirportTableAccess().getCityByOrigin(call.request.queryParameters["destination"] ?: ""),
             departureDate = call.request.queryParameters["departure_date"],
             returnDate = call.request.queryParameters["return_date"],
             adults = call.request.queryParameters["adults"],
@@ -69,16 +69,27 @@ fun Route.pagesRoutes() {
             return@get
         }
 
-        println("FLIGHT SEARCH DATA VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV")
-        println(search)
-        println("FLIGHT SEARCH DATA ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^")
+        val airports = AirportTableAccess()
+
+        // TODO: properly handle error when the inputted airport is not found, just isnt great
+        // this should be checked beforehand however, on the home page, since we should only ever pass error free
+        // form inputs to stages further up
+        val originAirportCode = airports.getAirportCodeByOrigin(search.origin) ?: ""
+        val destinationAirportCode = airports.getAirportCodeByOrigin(search.destination) ?: ""
+
+        println(originAirportCode)
+        println(destinationAirportCode)
+        
+        // println("FLIGHT SEARCH DATA VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV")
+        // println(search)
+        // println("FLIGHT SEARCH DATA ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^")
 
         // get outbound flight data
         val flightTable = FlightTableAccess()
-        val outboundFlights = flightTable.getFlightsAroundDate(search.origin, search.destination, LocalDate.parse(search.departureDate))
+        val outboundFlights = flightTable.getFlightsAroundDate(originAirportCode, destinationAirportCode, LocalDate.parse(search.departureDate))
 
         // get inbound flight data (for trip type = return)
-        val inboundFlights = flightTable.getFlightsAroundDate(search.destination, search.origin, LocalDate.parse(search.returnDate))
+        val inboundFlights = flightTable.getFlightsAroundDate(destinationAirportCode, originAirportCode, LocalDate.parse(search.returnDate))
 
         call.respond(PebbleContent("flight_search.peb", mapOf(
             "userSession" to session,
