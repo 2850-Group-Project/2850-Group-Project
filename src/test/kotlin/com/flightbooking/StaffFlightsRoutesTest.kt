235b176a85896c5151da3551a063a1f1634e07d7
dima <ed20d2i@leeds.ac.uk>
2026-03-21 21:50:07 +0000

    Refactor ApplicationTest into route-specific files and comment existing tests

--- src/test/kotlin/com/flightbooking/StaffFlightsRoutesTest.kt
+++ src/test/kotlin/com/flightbooking/StaffFlightsRoutesTest.kt
@@ -1,3 +1,21 @@
 package com.flightbooking
 
-class StaffFlightsRoutesTest
+import io.ktor.client.request.get
+import io.ktor.http.HttpHeaders
+import io.ktor.http.HttpStatusCode
+import io.ktor.server.testing.testApplication
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class StaffFlightsRoutesTest : IntegrationTestSupport() {
+    @Test
+    // Unauthenticated staff users should be sent to the staff login page.
+    fun unauthenticatedStaffFlightsRedirectsToStaffLogin() = testApplication {
+        configureApp()
+        val client = createClient { followRedirects = false }
+
+        val response = client.get("/staff/flights")
+        assertEquals(HttpStatusCode.Found, response.status)
+        assertEquals("/staff/login", response.headers[HttpHeaders.Location])
+    }
+}
