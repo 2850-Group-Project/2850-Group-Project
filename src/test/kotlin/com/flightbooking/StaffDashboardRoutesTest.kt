be5d8547a5432af0d4077b589204cdb5f0c1f0b8
dima <ed20d2i@leeds.ac.uk>
2026-03-23 02:44:13 +0000

    Added auth and staff dashboard test skeletons

--- src/test/kotlin/com/flightbooking/StaffDashboardRoutesTest.kt
+++ src/test/kotlin/com/flightbooking/StaffDashboardRoutesTest.kt
@@ -1,3 +1,20 @@
 package com.flightbooking
 
-class StaffDashboardRoutesTest
+import kotlin.test.Test
+
+class StaffDashboardRoutesTest : IntegrationTestSupport() {
+    @Test
+    // Unauthenticated staff users should be redirected to the staff login page.
+    fun unauthenticatedDashboardRedirectsToStaffLogin() {
+    }
+
+    @Test
+    // An authenticated staff user should be able to load the dashboard page.
+    fun authenticatedDashboardLoads() {
+    }
+
+    @Test
+    // A stale staff session should show a staff-not-found response.
+    fun dashboardShowsStaffNotFoundMessageWhenSessionUserIsMissing() {
+    }
+}
