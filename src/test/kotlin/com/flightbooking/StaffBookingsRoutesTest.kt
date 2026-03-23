6072b15a493924b0d2a14d8c24ed1e43aae1d241
dima <ed20d2i@leeds.ac.uk>
2026-03-23 01:32:28 +0000

    Add staff bookings test skeletons

--- src/test/kotlin/com/flightbooking/StaffBookingsRoutesTest.kt
+++ src/test/kotlin/com/flightbooking/StaffBookingsRoutesTest.kt
@@ -1,3 +1,25 @@
 package com.flightbooking
 
-class StaffBookingsRoutesTest
+import kotlin.test.Test
+
+class StaffBookingsRoutesTest : IntegrationTestSupport() {
+    @Test
+    // Staff should be able to create a booking from the bookings management page.
+    fun createBookingRedirectsWithSuccessMessage() {
+    }
+
+    @Test
+    // Staff should be able to update a booking status successfully.
+    fun updateBookingStatusRedirectsWithSuccessMessage() {
+    }
+
+    @Test
+    // Staff should be able to change the seat assignment for a booking.
+    fun changeSeatAssignmentRedirectsWithSuccessMessage() {
+    }
+
+    @Test
+    // Reassigning a booking to a different flight should handle seat state correctly.
+    fun reassignBookingFlightHandlesSeatCorrectly() {
+    }
+}
