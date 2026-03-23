b094be8d284409910063c5c9b9d2ee8b3b5977f7
dima <ed20d2i@leeds.ac.uk>
2026-03-23 02:51:22 +0000

    Added unauthenticated staff dashboard redirect test

--- src/test/kotlin/com/flightbooking/StaffDashboardRoutesTest.kt
+++ src/test/kotlin/com/flightbooking/StaffDashboardRoutesTest.kt
@@ -1,11 +1,22 @@
 package com.flightbooking
 
+import io.ktor.client.request.get
+import io.ktor.http.HttpHeaders
+import io.ktor.http.HttpStatusCode
+import io.ktor.server.testing.testApplication
 import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class StaffDashboardRoutesTest : IntegrationTestSupport() {
     @Test
     // Unauthenticated staff users should be redirected to the staff login page.
-    fun unauthenticatedDashboardRedirectsToStaffLogin() {
+    fun unauthenticatedDashboardRedirectsToStaffLogin() = testApplication {
+        configureApp()
+        val client = createClient { followRedirects = false }
+
+        val response = client.get("/staff/dashboard")
+        assertEquals(HttpStatusCode.Found, response.status)
+        assertEquals("/staff/login", response.headers[HttpHeaders.Location])
     }
 
     @Test
