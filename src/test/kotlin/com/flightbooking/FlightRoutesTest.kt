c2f74724ecb816c28f137ab1569607626d091429
dima <ed20d2i@leeds.ac.uk>
2026-03-25 20:26:24 +0000

    Added unauthenticated flight select redirect test

--- src/test/kotlin/com/flightbooking/FlightRoutesTest.kt
+++ src/test/kotlin/com/flightbooking/FlightRoutesTest.kt
@@ -1,11 +1,31 @@
 package com.flightbooking
 
+import io.ktor.client.request.forms.submitForm
+import io.ktor.http.HttpHeaders
+import io.ktor.http.HttpStatusCode
+import io.ktor.http.parameters
+import io.ktor.server.testing.testApplication
 import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class FlightRoutesTest : IntegrationTestSupport() {
     @Test
     // Unauthenticated users should be redirected to login when selecting a flight.
-    fun unauthenticatedFlightSelectRedirectsToLogin() {
+    fun unauthenticatedFlightSelectRedirectsToLogin() = testApplication {
+        configureApp()
+        val client = createClient { followRedirects = false }
+
+        val response = client.submitForm(
+            url = "/flights/select",
+            formParameters = parameters {
+                append("flightId", "1")
+                append("fareId", "1")
+                append("leg", "outbound")
+            }
+        )
+
+        assertEquals(HttpStatusCode.Found, response.status)
+        assertEquals("/login", response.headers[HttpHeaders.Location])
     }
 
     @Test
