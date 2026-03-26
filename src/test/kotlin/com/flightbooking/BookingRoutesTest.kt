841c397ebf249808f521d1285d2fe368fa072c16
dima <ed20d2i@leeds.ac.uk>
2026-03-26 12:51:05 +0000

    Added booking route test skeletons

--- src/test/kotlin/com/flightbooking/BookingRoutesTest.kt
+++ src/test/kotlin/com/flightbooking/BookingRoutesTest.kt
@@ -1,3 +1,25 @@
 package com.flightbooking
 
-class BookingRoutesTest
+import kotlin.test.Test
+
+class BookingRoutesTest {
+    // Check that submitting passenger details without logging in redirects to login.
+    @Test
+    fun unauthenticatedPassengerSubmitRedirectsToLogin() {
+    }
+
+    // Check that submitting passenger details without a booking session redirects home.
+    @Test
+    fun passengerSubmitRedirectsHomeWhenBookingSessionMissing() {
+    }
+
+    // Check that submitting one passenger saves their details in the booking session.
+    @Test
+    fun passengerSubmitStoresPassengerDataInBookingSession() {
+    }
+
+    // Check that submitting multiple passengers saves all of their details in the booking session.
+    @Test
+    fun passengerSubmitHandlesMultiplePassengers() {
+    }
+}
